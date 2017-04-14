ac58b4616e1d47a430c461199b7596fff9ab00a0
BuildTools <unconfigured@null.spigotmc.org>
2017-04-14 08:29:54 +0200

    Improve clear instances task

--- tasks/clear_instances.js
+++ tasks/clear_instances.js
@@ -1,10 +1,25 @@
 module.exports = () => {
 	const db_instances = DB.get('instances');
 
+	const delete_date = new Date();
+	delete_date.setTime(delete_date.getTime() - (1000 * 60 * 60));
+
 	db_instances.remove({
-	    upchecks: 0,
-	    downchecks: {
-	        $gt: 0
-	    }
+	    "upchecks": 0,
+	    "$or": [
+	    	{
+			    "downchecks": {
+			        "$gt": 0
+			    }
+	    	}, {
+	            "addedAt": {
+	                "$exists": false
+	            }
+	        }, {
+	            "addedAt": {
+	                "$lte": delete_date
+	            }
+	        }
+	    ]
 	});
 };
