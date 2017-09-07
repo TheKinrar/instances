b79a46f9c7dcc9d388f43c0289bd7837fae7c1bf
thekinrar <contact@thekinrar.fr>
2017-09-07 10:39:48 +0200

    Use User-Agent for every request

--- updater.js
+++ updater.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
 
+global.USER_AGENT = 'MastodonInstances (https://instances.social)';
+
 global.config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 global.DB = require('monk')(config.database);
 global.Request = require('request').defaults({
     headers: {
-        'User-Agent': 'MastodonInstances/1.0.0 (https://instances.mastodon.xyz)'
+        'User-Agent': USER_AGENT
     }
 });
 
