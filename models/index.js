664c9819f6734b6c2e8cfde305d86c4aabbfb4f3
thekinrar <contact@thekinrar.fr>
2018-03-24 01:32:48 +0100

    Add Sequelize

---
+++ models/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const Sequelize = require('sequelize');
+const config = require('../config');
+
+const sequelize = new Sequelize({
+    host: config.postgresql.host,
+    port: config.postgresql.port,
+    database: config.postgresql.database,
+    username: config.postgresql.username,
+    password: config.postgresql.password,
+
+    dialect: 'postgres',
+
+    operatorsAliases: false
+});
+
+module.exports = sequelize;
