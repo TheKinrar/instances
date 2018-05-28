e1a34aa3511205f8fa8297f71ac3b2dbac2ac157
thekinrar <contact@thekinrar.fr>
2018-05-28 15:55:55 +0200

    Create cron.js

--- cron.js
+++ cron.js
@@ -1,2 +1,27 @@
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 const Instance = require('./models/instance');
 
+(async () => {
+    let fiveMinutesBefore = new Date();
+    fiveMinutesBefore.setMinutes(fiveMinutesBefore.getMinutes() - 5);
+
+    let instances = await Instance.findAll({
+        where: {
+            latest_check: {
+                [Op.or]: {
+                    [Op.lt]: fiveMinutesBefore,
+                    [Op.eq]: null
+                }
+            }
+        }
+    });
+
+    console.log(`${instances.length} instances to check`);
+
+    for(let instance of instances) {
+        console.log(instance.name);
+    }
+
+    process.exit(0);
+})().catch(console.error);
