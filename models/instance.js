8a5e9d78f3c484625081e9c420e81e5d6e96a662
ThibG <thib@sitedethib.com>
2018-09-22 22:37:04 +0200

    Update instance.js

--- models/instance.js
+++ models/instance.js
@@ -113,8 +113,8 @@ Instance.hook('afterSave', async (instance) => {
     if(instance.raw_version)
         version = raw_version = instance.raw_version.replace(/\.$/, '');
 
-    let version_norc = version.replace(/\.?rc[0-9]/, '');
-    version_norc = version_norc.replace(/\+[0-9A-Za-z.-]*/, '');
+    let version_notag = version_norc.replace(/\+[0-9A-Za-z.-]*/, '');
+    let version_norc = version_notag.replace(/\.?rc[0-9]/, '');
 
     if(version === 'Mastodon::Version') {
         version = '1.3';
